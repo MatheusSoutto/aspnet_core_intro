edce3ec179fabeb7cc55973e445d922e743b7458
Matheus Soutto <matheussoutto27@gmail.com>
2022-06-20 09:56:44 -0300

    Changing Ratings to array

--- ContosoCrafts/Models/Product.cs
+++ ContosoCrafts/Models/Product.cs
@@ -12,7 +12,7 @@ public class Product
     public string Url { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
-    public IEnumerable<int> Ratings { get; set; }
+    public int[] Ratings { get; set; }
 
     public override string ToString() => JsonSerializer.Serialize<Product>(this);
 }
